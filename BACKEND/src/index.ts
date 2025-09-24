d034b7f8808ed6150ca03f336c9b654171755039
Java Practical <ahmedabdelhady0246@gmail.com>
2025-09-24 16:03:35 +0300

    chore(foodapp): initalzie backend node

---
+++ BACKEND/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
